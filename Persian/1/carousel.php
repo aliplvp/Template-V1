fd8de7784ceaf2a05c0869ce0232fc4a420af1aa
AliPlvP <AliPlvP@DESKTOP-QEPMJD7>
2024-04-15 19:44:08 +0330

    V2

--- Persian/1/carousel.php
+++ Persian/1/carousel.php
@@ -1,27 +1,3 @@
-<!-- Carousel -->
-<div class="carousel" style="overflow: hidden; direction: ltr;">
-    <div class="reel" id="banner">
-
-        <article>
-            <a href="https://aliplvp.ir/template/Persian/1" class="image featured"><img src="images/resized_31788_593.jpg" alt="" /></a>
-            <header>
-                <h3><a href="">تاریخ حرم بقیع از تخریب تا امروز</a></h3>
-            </header>
-        </article>
-
-        <article>
-            <a href="https://aliplvp.ir/template/Persian/2" class="image featured"><img src="images/5401012.jpg" alt="" /></a>
-            <header>
-                <h3><a href="#">چگونگی تایید اصالت عکس قبرستان بقیع</a></h3>
-            </header>
-        </article>
-
-        <article>
-            <a href="https://aliplvp.ir/template/Persian/3" class="image featured"><img src="images/714728_528.jpg" alt="" /></a>
-            <header>
-                <h3><a href="#">چگونگی الهام از نوع معماری و مناره</a></h3>
-            </header>
-        </article>
-
-    </div>
+<div class="cycle-slideshow m-3" data-cycle-slides="> div" data-cycle-fx="carousel" data-cycle-timeout="0" data-cycle-carousel-visible="5">
+    <!-- Card placeholders will be added here dynamically -->
 </div>
